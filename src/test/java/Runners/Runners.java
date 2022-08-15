910c53e3e2d1e729c5d1d5a8e9a568f3973efa53
MohammedHendy94 <MohammedHendy94@gmail.com>
2022-08-15 13:53:26 +0200

    third commit

--- src/test/java/Runners/Runners.java
+++ src/test/java/Runners/Runners.java
@@ -6,7 +6,7 @@ import io.cucumber.junit.CucumberOptions;
 import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(features = "src/test/java/Features", glue = "Tests")
+@CucumberOptions(features = "src/test/java/Features", glue = "Tests", plugin = "html:target/reports/report.html")
 public class Runners {
 
 
